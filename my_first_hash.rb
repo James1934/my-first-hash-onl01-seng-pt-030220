8224107c25aa686c4ad265a529343340dcb66941
Bartholomew Fountain <baertholomew@gmail.com>
2020-03-12 17:32:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,4 +2,5 @@ def my_hash
   my_hash={"name"=>"Alena", "sex"=>"female", "favorite desert"=>"cheesecake"}
 end
  
- 
+ shipping_manifest = {}
+shipping_manifest={"whale bone corsets"=>5,"porcelain vases" => 2, "oil paintings" => 3}
